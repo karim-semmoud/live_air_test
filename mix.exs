74f6726773748fb0e0f996192b07a00ef5b225b8
karimsemmoud <karim.semmoud@netoum.com>
2024-01-28 20:23:32 +0700

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -50,9 +50,7 @@ defmodule LiveAir.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"},
-      {:moon, "~> 2.80.0"}
-    ]
+      {:plug_cowboy, "~> 2.5"}    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
